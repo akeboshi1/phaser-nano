79180dc8b03a845320fc4a0c3fcab4d9d4673d5f
Richard Davey <rich@photonstorm.com>
2020-03-23 16:24:55 +0000

    Update IContainerChild.ts

--- src/gameobjects/IContainerChild.ts
+++ src/gameobjects/IContainerChild.ts
@@ -3,6 +3,7 @@ import * as Components from '../components';
 export default interface IContainerChild extends
     Components.IDirtyComponent,
     Components.IParentComponent,
-    Components.IRenderableComponent,
-    Components.ITransformComponent
-    {}
+    Components.IRenderableComponent
+    {
+        updateTransform (): this;
+    }
