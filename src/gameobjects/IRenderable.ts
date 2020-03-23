80a505b58369d80e09039b1018785b011946765b
Richard Davey <rich@photonstorm.com>
2020-03-23 16:25:02 +0000

    Update IRenderable.ts

--- src/gameobjects/IRenderable.ts
+++ src/gameobjects/IRenderable.ts
@@ -1,6 +1,10 @@
 import { ITextureComponent, IRenderableComponent } from '../components';
+import WebGLRenderer from '../renderer/WebGLRenderer';
+import MultiTextureQuadShader from '../renderer/MultiTextureQuadShader';
 
 export default interface IRenderable extends
     ITextureComponent,
     IRenderableComponent
-    {}
+    {
+        renderWebGL (renderer: WebGLRenderer, shader: MultiTextureQuadShader, startActiveTexture: number): void;
+    }
