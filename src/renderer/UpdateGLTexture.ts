e0908137b543ef31dbbd69a6ea7b952f668fdbd9
Richard Davey <rich@photonstorm.com>
2020-03-24 17:01:27 +0000

    Create UpdateGLTexture.ts

---
+++ src/renderer/UpdateGLTexture.ts
@@ -0,0 +1,14 @@
+export default function UpdateGLTexture (gl: WebGLRenderingContext, source: TexImageSource, dstTexture: WebGLTexture, flipY: boolean = false)
+{
+    const width = source.width;
+    const height = source.height;
+
+    if (width > 0 && height > 0)
+    {
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, dstTexture);
+
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, flipY);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, source);
+    }
+}
