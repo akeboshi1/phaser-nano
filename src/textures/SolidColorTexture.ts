44c3b4ddae9e4da6344af1c30c78fff94dce5f7a
Richard Davey <rich@photonstorm.com>
2020-03-24 17:02:09 +0000

    Add default color

--- src/textures/SolidColorTexture.ts
+++ src/textures/SolidColorTexture.ts
@@ -1,7 +1,7 @@
 import CreateCanvas from './CreateCanvas';
 import Texture from './Texture';
 
-export default function SolidColorTexture (color: string, width: number = 32, height: number = 32): Texture
+export default function SolidColorTexture (color: string = 'rgba(0,0,0,0)', width: number = 32, height: number = 32): Texture
 {
     const ctx = CreateCanvas(width, height);
 
