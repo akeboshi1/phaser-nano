83ab6e67ce4af8a34a503474d3661579dc322d0a
Richard Davey <rich@photonstorm.com>
2020-03-25 01:51:14 +0000

    Update IGameConfig.ts

--- src/IGameConfig.ts
+++ src/IGameConfig.ts
@@ -3,6 +3,7 @@ import IScene from './scenes/IScene';
 export default interface IGameConfig {
     width?: number;
     height?: number;
+    resolution?: number;
     parent?: string | HTMLElement;
     backgroundColor?: number;
     scene?: IScene | IScene[] | any | any[];
